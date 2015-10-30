2b24ad4d6f5f4d9b16cf68ba891a6f7d9a0e6a25
Thorsten Kugelberg <tk@kugelberg.de>
2015-10-30 21:47:45 +0100

    Create Module

---
+++ Ivona/ivona.php
@@ -0,0 +1,132 @@
+<?php
+
+///////////////////////////////////////////////////////////////////////////////////////
+//   IVONA_TTS             ////////////////////////////////////////////////////////////
+//    by Titus 15.10.2015  ////////////////////////////////////////////////////////////
+//    enhanced by Thorsten Kugelberg 30.10.2015 ///////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////
+
+class IVONA_TTS{
+    static  $utc_tz;
+    private $access_key  = "";
+    private $secret_key  = "";
+    private $language    = "";
+    private $voice       = "";
+    private $rate        = "";
+    private $volume      = "";
+    private $hash_exists = "";
+
+    public function __construct( $access_key, $secret_key , $language="de-DE", $voice="Marlene", $rate="medium", $volume="loud"){
+        $this->utc_tz      = new \DateTimeZone( 'UTC' );
+        $this->access_key  = $access_key;
+        $this->secret_key  = $secret_key;
+        $this->language    = $language;
+        $this->voice       = $voice;
+        $this->rate        = $rate;
+        $this->volume      = $volume;
+        $this->hash_exists = function_exists('hash_hmac');
+    }
+
+    public function save_mp3($text, $filename) {
+            $mp3 = $this->get_mp3($text);
+            file_put_contents($filename, $mp3);
+     }
+
+    public function get_mp3( $text )
+    {
+        $payload = json_encode(array( ('Input')      => array(('Data')     => utf8_encode($text)),
+                                      ('Parameters') => array(('Rate')     => $this->rate,
+                                                              ('Volume')   => $this->volume),
+                                      ('Voice')      => array(('Name')     => $this->voice,
+                                                              ('Language') => $this->language )));
+       
+        $datestamp                = new \DateTime( "now", $this->utc_tz );
+        $longdate                 = $datestamp->format( "Ymd\\THis\\Z");
+        $shortdate                = $datestamp->format( "Ymd" );
+        $ksecret                  = 'AWS4' . $this->secret_key;
+        if($this->hash_exists){
+          $params                   = array( 'host'                 => 'tts.eu-west-1.ivonacloud.com',
+                                             'content-type'         => 'application/json',
+                                             'x-amz-content-sha256' => hash( 'sha256', $payload, false ),
+                                             'x-amz-date'           => $longdate );
+        }else{
+          $hash_command             = "php -r \"print(hash( 'sha256', '".str_replace('"','\"',$payload)."', false ));\"";
+          $params                   = array( 'host'                 => 'tts.eu-west-1.ivonacloud.com',
+                                             'content-type'         => 'application/json',
+                                             'x-amz-content-sha256' => exec($hash_command),
+                                             'x-amz-date'           => $longdate );
+        }
+        $canonical_request        = $this->createCanonicalRequest( $params, $payload );
+        if($this->hash_exists){
+          $signed_request         = hash( 'sha256', $canonical_request, false );
+        }else{
+          $hash_command           = "php -r \"print(hash( 'sha256', '".$canonical_request."', false ));\"";
+          $signed_request         = exec($hash_command);
+        }
+        $sign_string              = "AWS4-HMAC-SHA256\n{$longdate}\n$shortdate/eu-west-1/tts/aws4_request\n" . $signed_request;
+        if($this->hash_exists){
+          $signature              = hash_hmac( 'sha256', $sign_string, hash_hmac( 'sha256', 'aws4_request', hash_hmac( 'sha256', 'tts', hash_hmac( 'sha256', 'eu-west-1', hash_hmac( 'sha256', $shortdate, $ksecret, true ) , true ) , true ), true ));
+        }else{
+          $signature_command      = "php -r \"print(hash_hmac( 'sha256', '".$sign_string."', hash_hmac( 'sha256', 'aws4_request', hash_hmac( 'sha256', 'tts', hash_hmac( 'sha256', 'eu-west-1', hash_hmac( 'sha256', '".$shortdate."', '".$ksecret."', true ) , true ) , true ), true ) ) );\"";
+          $signature              = exec($signature_command);;
+        }
+        $params['Authorization']  = "AWS4-HMAC-SHA256 Credential=" . $this->access_key . "/$shortdate/eu-west-1/tts/aws4_request, " .
+                                    "SignedHeaders=content-type;host;x-amz-content-sha256;x-amz-date, " .
+                                    "Signature=$signature";
+        $params['content-length'] = strlen( $payload ) ;
+        /*
+         * Execute Crafted Request
+         */
+        $url    = "https://tts.eu-west-1.ivonacloud.com/CreateSpeech";
+        $ch     = curl_init();
+        $curl_headers = array();
+        foreach( $params as $p => $k )
+            $curl_headers[] = $p . ": " . $k;
+        curl_setopt($ch, CURLOPT_URL,$url);
+        curl_setopt($ch, CURLOPT_POST,1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $curl_headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+        curl_setopt($ch, CURLOPT_TCP_NODELAY, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false );
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false );
+        // debug opts
+        {
+            curl_setopt($ch, CURLOPT_VERBOSE, true);
+            $verbose = fopen('php://temp', 'rw+');
+            curl_setopt($ch, CURLOPT_STDERR, $verbose);
+            $result = curl_exec($ch); // raw result
+            rewind($verbose);
+            $verboseLog = stream_get_contents($verbose);
+            #echo "Verbose information:\n<pre>", htmlspecialchars($verboseLog), "</pre>\n";
+        }
+        return $result;
+    }
+    
+    private function createCanonicalRequest( Array $params, $payload )
+    {
+        $canonical_request      = array();
+        $canonical_request[]    = 'POST';
+        $canonical_request[]    = '/CreateSpeech';
+        $canonical_request[]    = '';
+        $can_headers            = array(
+          'host' => 'tts.eu-west-1.ivonacloud.com'
+        );
+        foreach( $params as $k => $v )
+            $can_headers[ strtolower( $k ) ] = trim( $v );
+        uksort( $can_headers, 'strcmp' );
+        foreach ( $can_headers as $k => $v )
+            $canonical_request[] = $k . ':' . $v;
+        $canonical_request[] = '';
+        $canonical_request[] = implode( ';', array_keys( $can_headers ) );
+        if($this->hash_exists){
+          $canonical_request[] = hash( 'sha256', $payload, false );
+        }else{
+          $hash_command        = "php -r \"print(hash( 'sha256', '".str_replace('"','\"',$payload)."', false ));\"";
+          $canonical_request[] = exec($hash_command);
+        }
+        $canonical_request = implode( "\n", $canonical_request );
+        return $canonical_request;
+    }
+} 
+?>
---
+++ Ivona/module.php
@@ -0,0 +1,106 @@
+<?
+class Ivona extends IPSModule
+{
+    
+    public function Create()
+    {
+        //Never delete this line!
+        parent::Create();
+        
+        //These lines are parsed on Symcon Startup or Instance creation
+        //You cannot use variables here. Just static values.
+        $this->RegisterPropertyString("accessKey", "");
+        $this->RegisterPropertyString("secretKey", "");
+        $this->RegisterPropertyString("language", "");
+        $this->RegisterPropertyString("voice", "");
+        $this->RegisterPropertyString("rate", "");
+        $this->RegisterPropertyString("volume", "");
+        $this->RegisterPropertyString("defaultPath", "");
+        $this->RegisterPropertyBoolean("deleteFiles", true);
+        $this->RegisterPropertyInteger("deleteMinutes", 15);
+
+    }
+    
+    public function ApplyChanges()
+    {
+        //Never delete this line!
+        parent::ApplyChanges();
+        
+        // End Register variables and Actions
+        
+        $deleteFilesScript = '<?
+$path = IPS_GetProperty(IPS_GetParent($_IPS["SELF"]), "defaultPath");
+
+if($path && IPS_GetProperty(IPS_GetParent($_IPS["SELF"]), "deleteFiles")){  
+
+  $minutes = IPS_GetProperty(IPS_GetParent($_IPS["SELF"]), "deleteMinutes");
+  if ($handle = opendir($path)) {
+
+    while (false !== ($file = readdir($handle))) {
+      if ((time()-filectime($path.'/'.$file)) < $minutes*60) {  // 86400 = 60*60*24
+        if (preg_match('/\.mp3$/i', $file)) {
+          unlink($path.'/'.$file);
+        }
+      }
+    }
+  }
+}
+?>';
+
+        $deleteScriptID = @$this->GetIDForIdent("_deleteFiles");
+        if ( $deleteScriptID === false ){
+          $deleteScriptID = $this->RegisterScript("_deleteFiles", "_deleteFiles", $deleteFilesScript, 99);
+        }else{
+          IPS_SetScriptContent($deleteScriptID, $deleteFilesScript);
+        }
+
+        IPS_SetHidden($deleteScriptID,true);
+        IPS_SetScriptTimer($deleteScriptID, 300); 
+
+
+        // End add scripts for regular status and grouping updates
+    }
+    
+    /**
+    * This function will be available automatically after the module is imported with the module control.
+    * Using the custom prefix this function will be callable from PHP and JSON-RPC through:
+    *
+    * SNS_Play($id);
+    *
+    */
+    public function getMP3($text)
+    {
+        include_once(__DIR__ . "/ivona.php");
+        (new new IVONA_TTS( $this->ReadPropertyString("accessKey") ,
+                            $this->ReadPropertyString("secretKey") ,
+                            $this->ReadPropertyString("language") ,
+                            $this->ReadPropertyString("voice") ,
+                            $this->ReadPropertyString("rate") ,
+                            $this->ReadPropertyString("volume")))->get_mp3($text);
+        
+    }
+
+    public function saveMP3($text,$file,$path='NONE')
+    {
+
+        if($path === 'NONE'){
+          $path = $this->ReadPropertyString("defaultPath");
+        }
+
+        if($path === ''){
+         $path = '/tmp';
+        }
+
+        $file = $path . "/" . $file;
+
+        include_once(__DIR__ . "/ivona.php");
+        (new new IVONA_TTS( $this->ReadPropertyString("accessKey") ,
+                            $this->ReadPropertyString("secretKey") ,
+                            $this->ReadPropertyString("language") ,
+                            $this->ReadPropertyString("voice") ,
+                            $this->ReadPropertyString("rate") ,
+                            $this->ReadPropertyString("volume")))->save_mp3($text, $file);
+        
+    }
+}
+?>
